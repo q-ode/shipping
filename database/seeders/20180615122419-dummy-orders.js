301dc3d71aa52eaad52e5a5bf1abf0f4f30ead59
Adebayo Adesanya <bayodapo@gmail.com>
2018-06-15 14:04:23 +0100

    chore: add seeder to generate dummy order records

---
+++ database/seeders/20180615122419-dummy-orders.js
@@ -0,0 +1,51 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('orders', [
+      {
+        customerName: 'Peter Lustig',
+        customerAddress: 'Steindamm 80 ',
+        item: 'Macbook',
+        price: 1700,
+        currency: 'EUR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        customerName: 'John Smith',
+        customerAddress: 'Reeperbahn 153 ',
+        item: 'Macbook',
+        price: 1700,
+        currency: 'EUR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        customerName: 'Ted Justice',
+        customerAddress: 'Lagerstrasse 11 ',
+        item: 'Book "Guide to Hamburg"',
+        price: '20',
+        currency: 'EUR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        customerName: 'Peter Lustig',
+        customerAddress: 'Steindamm 80',
+        item: 'Book "Cooking 101"',
+        price: 10,
+        currency: 'EUR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },{
+        customerName: 'Peter Lustig',
+        customerAddress: 'Steindamm 80',
+        item: 'Inline Skates',
+        price: 75,
+        currency: 'EUR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('orders', null, {});
+  }
+};
